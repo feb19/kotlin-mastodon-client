42ca54b5796559cd2964c4645bb9822ae90ea67c
Nobuhiro Takahashi <nt@feb19.jp>
2017-04-19 01:54:16 +0900

    first

---
+++ app/src/main/java/jp/feb19/kotlin_sample/MainActivity.kt
@@ -0,0 +1,15 @@
+package jp.feb19.kotlin_sample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("Kotlin Sample", "hage")
+    }
+}
